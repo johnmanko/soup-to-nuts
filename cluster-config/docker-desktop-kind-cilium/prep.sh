2096bef27be0d59062d9127c07ee05059651d2e6
John Manko <github@johnmanko.com>
2024-12-15 18:34:23 -0500

    remove duplicate docs

--- cluster-config/docker-desktop-kind-cilium/prep.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Kubernetes Cloud Provider for KIND
-# https://github.com/kubernetes-sigs/cloud-provider-kind?tab=readme-ov-file#install
-
-kubectl label node kind-control-plane node.kubernetes.io/exclude-from-external-load-balancers-node/kind-control-plane unlabeled
